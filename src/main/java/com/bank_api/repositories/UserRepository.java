9dd61c773b749053ffc681b8e45cd29a0043084d
Bruno Pressi <brunopressi2012@gmail.com>
2025-07-29 15:54:47 -0300

    feat: add User Repository

---
+++ src/main/java/com/bank_api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bank_api.repositories;
+
+import com.bank_api.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
